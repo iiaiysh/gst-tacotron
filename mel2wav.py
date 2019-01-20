cfa34f8d0c23929f6b69fb8b78cc52772de8d581
shihang <shihang.yu@inceptioniai.org>
2019-01-20 15:12:39 +0400

    add argparse in mel2wav.py

--- mel2wav.py
+++ mel2wav.py
@@ -1,5 +1,13 @@
 import numpy as np
 from util import audio
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--mel',requried=True)
+parser.add_argument('--wav',requried=True)
+args = parser.parse_args()
+
+
 linear_target = np.load(args.mel)
 
 waveform_target = audio.inv_spectrogram(linear_target.T)
